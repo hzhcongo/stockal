dfda0a9fd6a72e82600116e01eb0b8c956cf7348
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2021-12-19 22:08:22 +0800

    master - Remove filetypes

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -17,10 +17,10 @@ import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from './listItems.tsx';
-import Chart from './Chart.tsx';
-import Deposits from './Deposits.tsx';
-import Orders from './Orders.tsx';
+import { mainListItems, secondaryListItems } from './listItems';
+import Chart from './Chart';
+import Deposits from './Deposits';
+import Orders from './Orders';
 
 function Copyright(props: any) {
   return (
