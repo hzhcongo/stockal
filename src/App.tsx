916f6b5acd737771280babbd800fe761947b438c
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2021-12-19 22:34:15 +0800

    master - typescripted

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './Components/Dashboard';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  return (<Dashboard></Dashboard>);
+}
+
+export default App;
