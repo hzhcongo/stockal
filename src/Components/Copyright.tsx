983c6f8e5cfb599da4dc4f673baf1e9307b299f5
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2021-12-19 23:47:59 +0800

    master - Update Copyright texts

--- src/Components/Copyright.tsx
+++ src/Components/Copyright.tsx
@@ -4,9 +4,9 @@ import Link from '@mui/material/Link';
 export default function Copyright(props: any) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-            Your Website
+        {'Copyright © Stockal and '}
+        <Link color="inherit" href="https://www.linkedin.com/in/ivanhengzehao/">
+            Ivan Heng Ze Hao
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
